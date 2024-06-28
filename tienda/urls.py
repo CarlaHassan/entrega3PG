3fd4b5dc87c7ebbef9e455275a6afcef87c576e3
Carla <ca.hassan@duocuc.cl>
2024-06-28 15:32:52 -0400

    urls index

--- tienda/urls.py
+++ tienda/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -23,6 +23,8 @@ urlpatterns = [
     path('tienda2/' ,include('tienda2.urls')),
     path('productos/', views.productos, name='productos'),
     path('formulario/', views.formulario, name='formulario'),
-    path('galeria/', views.galeria, name='galeria')
+    path('galeria/', views.galeria, name='galeria'),
+    path('', include('tu_aplicacion.urls'))
 ]
+
 
